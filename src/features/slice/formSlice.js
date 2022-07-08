b2e161a6b8ec2e050e6de80e1e7f77d798e7130f
nbvishal@noborderz.com <vishal.thoughtwin@gmail.com>
2022-07-08 15:16:17 +0530

    form Data update done without API

--- src/features/slice/formSlice.js
+++ src/features/slice/formSlice.js
@@ -4,20 +4,33 @@ export const formSlice = createSlice({
     name:'form',
     initialState:{
         formData:[],
-        updateData:[],
-        deleteData:[]
     },
     reducers:{
         form_data : (state,action) => {
-            state.formData.push(action.payload)
+            state.formData.push(action.payload);   
         },
-        delete_data : (state,action) => {
-            console.log(state)
-            state.formData.push(action.payload)
-        },
-        updt_data : (state,action) => {
-            console.log(state)
-            state.updateData.push(action.payload)
+
+        updt_data(state, action) {
+            const { id, name, sname } = action.payload;
+            const existingUser = state.formData.find((user) => user.id === id);
+            console.log(JSON.parse(existingUser.id)+1)
+            console.log(id)
+            if (existingUser) {
+              existingUser.name = name;
+              existingUser.sname = sname;
+            }
+          },
+          
+        delete_data(state, action) {
+            console.log(state.formData)
+            const { id, name, sname } = action.payload;
+
+            const data = state.formData.find((user) => user.id === id);
+            console.log(data)
+            console.log(id)
+            if (data) {
+                return state.formData.filter(user=>user.id !== id);
+            }
         }
     },
     
@@ -25,6 +38,114 @@ export const formSlice = createSlice({
 
 export default formSlice.reducer;
 
-export const {form_data} = formSlice.actions
-export const {delete_data} = formSlice.actions
-export const {updt_data} = formSlice.actions
+export const {form_data,delete_data,updt_data} = formSlice.actions
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const { id, name, sname } = action.payload;
+// const data = state.formData.find((user) => user.id === id);
+// if (data) {
+//         data.name = name;
+//         data.sname = sname;
+//         }
+// if (data) {
+//   return state.formData.filter(user=>user.id !== id);
+// }
+
+
+  // console.log('existingUser',existingUser)
+            // if (existingUser) {
+            //     existingUser.id = '';
+            //     existingUser.name = name;
+            //     existingUser.sname = sname;
+            //   }
+
+            // if (existingUser) {
+            //   return state.formData.filter(user=>user.id !== id);
+            // }
+            // console.log('existingUser',existingUser)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//             import { createSlice } from "@reduxjs/toolkit";
+
+// export const formSlice = createSlice({
+//     name:'form',
+//     initialState:{
+//         formData:[],
+//     },
+//     reducers:{
+//         form_data : (state,action) => {
+//             state.formData.push(action.payload);   
+//         },
+
+//         updt_data(state, action) {
+//             const { id, name, sname } = action.payload;
+//             const existingUser = state.formData.find((user) => user);
+//             console.log('-=--=-=-=-updt====',existingUser.id)
+//             if (existingUser) {
+//               existingUser.name = name;
+//               existingUser.sname = sname;
+//             }
+//           },
+          
+//         delete_data(state, action) {
+//             console.log(state.formData)
+//             const { id, name, sname } = action.payload;
+//             console.log('++++++++++++',id)
+//             const data = state.formData.find((user) => user.id === id);
+//             if (data) {
+//                 return state.formData.filter(user=>user.id !== id);
+//             }
+//         }
+//     },
+    
+// })
+
+// export default formSlice.reducer;
+
+// export const {form_data,delete_data,updt_data} = formSlice.actions
+
+
+
--- src/pages/simple_crud/Form.js
+++ src/pages/simple_crud/Form.js
@@ -1,16 +1,19 @@
-import React, { useState } from 'react';
+import React, { useId, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { delete_data, form_data, updt_data } from '../../features/slice/formSlice';
 import Header from '../layouts/Header'
 export default function Form() {
 
     const [data,setData] = useState([])
     const [updt,setUpdt] = useState({
+        id:'',
         name:'',
         sname:''
     })
+    
     const [del,setDel] = useState({
+        id:'',
         name:'',
         sname:''
     })
@@ -18,9 +21,7 @@ export default function Form() {
     const dispatch = useDispatch();
 
     const state = useSelector(state=>state)
-    console.log(state.form)
-
-    const navigate = useNavigate()
+    console.log(state.form.formData)
 
     const changeValue = (e) => {
         const {name,value} = e.target
@@ -35,6 +36,7 @@ export default function Form() {
         dispatch(form_data(data))
         setData({
             ...data,
+            id:'',
             name:'',
             sname:''
         })
@@ -43,64 +45,50 @@ export default function Form() {
 
     const deleteBtn = (e,idx) => {
         e.preventDefault();
-           
-        let d = state.form.deleteData
-        console.log(d.idx)
-      let value = d.filter((c,i,a)=>{
-           return (
-           a.indexOf(c) == idx
-           )
-       });
-       console.log(value) 
-       setDel({
-           name:value.name,
-           sname:value.sname
-       })
-        // dispatch(delete_data(del))    
-     
-    }
-
-    // const deleteBtn = (e,idx) => {
-    //     // e.preventDefault();
-    //     console.log('ok')
-    //     let id = e.target.value
-    //     console.log('------id',id)
-    //     console.log('idx======',idx)
-    //     // let del = e.target.closest('tr')
-    //     // del.remove(state.form)
-    //     // data.filter((_, i) => id !== idx);
-
-    //     let myNewData = state.form.filter((_,cv)=> {
-    //         console.log(cv)
-    //         if(cv.id !== idx){
-    //             console.log(_)
-    //             console.log("ok");
-    //         }else{
-    //             console.log('not ok')
-    //         }
-    //     })
-    //     console.log(myNewData)
-        
-    // }
-    
-
-    const editBtn = (e,idx) => {
-        console.log(state.form[idx])
         let d = state.form.formData
       let value = d.filter((c,i,a)=>{
            return (
            a.indexOf(c) == idx
            )
        });
-       let name = value[0].name
-       let sname = value[0].sname
        console.log(value[0]) 
+    //    setDel({
+    //        ...del,
+    //        id:idx+1,
+    //        name:'',
+    //        sname:''
+    //    })
+    //    console.log(del)
+        dispatch(delete_data(value))    
+     
+    }
+
+    const editBtn = (e,idx) => {
+        console.log(state.form.formData[idx].name)
+        let d = state.form.formData
+      let value = d.filter((c,i,a)=>{
+           return (
+           a.indexOf(c) == idx
+           )
+       });
+       value.map(cv=>
+        console.log(value))
+        let id = value[0].id;
+        let name = value[0].name;
+        let sname = value[0].sname;
+        console.log(idx)
        setUpdt({
            ...updt,
+           id:id,
            name:name,
            sname:sname
        })   
-       console.log(updt)  
+
+    // console.log(value[0])
+    // updt.id = value[0].id
+    // updt.name = value[0].name
+    // updt.sname = value[0].sname
+    //    console.log(updt)  
     }
 
     const updateValue = (e) => {
@@ -109,14 +97,16 @@ export default function Form() {
             ...updt,
             [name]:value
         })
-        console.log(updt)
+        // console.log(updt)
     }
 
     const updtBtn = (e) => {
         e.preventDefault();
         dispatch(updt_data(updt))
+        console.log(updt)
         setUpdt ({
             ...updt,
+            id:'',
             name:'',
             sname:''
         })
@@ -173,7 +163,7 @@ export default function Form() {
       <div className='mx-auto w-25 mt-5 border mb-5'>
           <form className='p-5'>
               <h3 className='text-center mb-4'>Form Data</h3>
-              {/* <input type='number' className='form-control id' name="id" value={data?.id ?? ''}  onChange={changeValue} placeholder='Enter Id' /><br />  */}
+              <input type='number' className='form-control id' name="id" value={data?.id ?? ''}  onChange={changeValue} placeholder='Enter Id' /><br /> 
               <input type='text' className="form-control name" name='name' value={data?.name ?? ''} onChange={changeValue} placeholder='Enter Name'/><br />
               <input type='text' className="form-control sname" name='sname' value={data?.sname ?? ''} onChange={changeValue} placeholder='Enter Surname' /><br />
               <button className='btn btn-success' onClick={submitBtn}>Submit</button>
@@ -183,3 +173,219 @@ export default function Form() {
     </>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useId, useState } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { useLocation, useNavigate } from 'react-router-dom';
+// import { delete_data, form_data, updt_data } from '../../features/slice/formSlice';
+// import Header from '../layouts/Header'
+// export default function Form() {
+
+//     const [data,setData] = useState([])
+//     const [updt,setUpdt] = useState({
+//         id:'',
+//         name:'',
+//         sname:''
+//     })
+//     const [del,setDel] = useState({
+//         id:'',
+//         name:'',
+//         sname:''
+//     })
+
+//     const dispatch = useDispatch();
+
+//     const state = useSelector(state=>state)
+//     console.log(state.form.formData)
+
+//     const changeValue = (e) => {
+//         const {name,value} = e.target
+//         setData({
+//             ...data,
+//             [name]:value
+//         })
+//         // console.log(data)
+//     }
+//     const submitBtn = (e) => {
+//         e.preventDefault();
+//         dispatch(form_data(data))
+//         setData({
+//             ...data,
+//             id:'',
+//             name:'',
+//             sname:''
+//         })
+//         // console.log(data)
+//     }
+
+//     const deleteBtn = (e,idx) => {
+//         e.preventDefault();
+//         let d = state.form.formData
+//       let value = d.filter((c,i,a)=>{
+//            return (
+//            a.indexOf(c) == idx
+//            )
+//        });
+//        console.log(value[0]) 
+//     //    setDel({
+//     //        ...del,
+//     //        id:idx+1,
+//     //        name:'',
+//     //        sname:''
+//     //    })
+//        console.log(del)
+//         dispatch(delete_data(data))    
+     
+//     }
+
+//     const editBtn = (e,idx) => {
+//         console.log(state.form.formData[idx].name)
+//         let d = state.form.formData
+//       let value = d.filter((c,i,a)=>{
+//            return (
+//            a.indexOf(c) == idx
+//            )
+//        });
+//        value.map(cv=>
+//         console.log(value))
+//         let name = value[0].name;
+//         let sname = value[0].sname;
+//        setUpdt({
+//            ...updt,
+//            id:idx+1,
+//            name:name,
+//            sname:sname
+//        })   
+//        console.log(updt)  
+//     }
+
+//     const updateValue = (e) => {
+//         const {name , value} = e.target
+//         setUpdt({
+//             ...updt,
+//             [name]:value
+//         })
+//         console.log(updt)
+//     }
+
+//     const updtBtn = (e) => {
+//         e.preventDefault();
+//         dispatch(updt_data(updt))
+//         setUpdt ({
+//             ...updt,
+//             name:'',
+//             sname:''
+//         })
+//     }
+
+//   return (
+//     <>
+//         <Header />
+
+//         {
+//         //   updt.length > 0 && 
+//           <div className='mx-auto w-25 mt-5 border mb-5'>
+//           <form className='p-5'>
+//               <h3 className='text-center mb-4'>Update Form Data</h3>
+//               {/* <input type='number' className='form-control id' name="id" value={data?.id ?? ''}  onChange={changeValue} placeholder='Enter Id' /><br />  */}
+//               <input type='text' className="form-control name" name='name' value={updt?.name ?? ''} onChange={updateValue } placeholder='Enter Name'/><br />
+//               <input type='text' className="form-control sname" name='sname' value={updt?.sname ?? ''} onChange={updateValue} placeholder='Enter Surname' /><br />
+//               <button className='btn btn-success' onClick={updtBtn}>Submit</button>
+//             </form>
+//         </div> 
+//       }
+//         <h4 className='text-center mt-5'>Form Data</h4>
+      
+//       <div className='container-fluid d-flex justify-content-center mt-5'>
+//         <table className=' container-fluid'>
+//             <thead>
+//                 <tr className='row'>
+//                     <th className='col-3 text-center border p-1'>S.No.</th>
+//                     <th className='col-3 text-center border p-1'>Name</th>
+//                     <th className='col-3 text-center border p-1'>Surname</th>
+//                     <th className='col-3 text-center border p-1'>Action</th>
+//                 </tr>
+//             </thead>
+//             <tbody>
+//                     {
+//                         state.form.formData.map((cv,idx,arr)=>{
+//                             // console.log(cv);
+//                             return (
+//                                 <tr className='row' key={idx}>
+//                                     <td className='col-3 text-center border p-2' >{idx+1}</td>
+//                                     <td className='col-3 text-center border p-2' >{cv.name}</td>
+//                                     <td className='col-3 text-center border p-2' >{cv.sname}</td>
+//                                     <td className='col-3 text-center border p-2'>
+//                                         <button className='btn btn-success' value={idx+1} onClick={(e)=>{editBtn(e,idx)}}>Edit</button>&nbsp;
+//                                         <button className='btn btn-danger' value={idx+1} onClick={(e)=>{deleteBtn(e,idx)}}>Delete</button>
+//                                     </td>
+//                                 </tr>
+//                             )
+//                         })
+//                     }
+//             </tbody>
+//         </table>
+//       </div>
+//       <div className='mx-auto w-25 mt-5 border mb-5'>
+//           <form className='p-5'>
+//               <h3 className='text-center mb-4'>Form Data</h3>
+//               <input type='number' className='form-control id' name="id" value={data?.id ?? ''}  onChange={changeValue} placeholder='Enter Id' /><br /> 
+//               <input type='text' className="form-control name" name='name' value={data?.name ?? ''} onChange={changeValue} placeholder='Enter Name'/><br />
+//               <input type='text' className="form-control sname" name='sname' value={data?.sname ?? ''} onChange={changeValue} placeholder='Enter Surname' /><br />
+//               <button className='btn btn-success' onClick={submitBtn}>Submit</button>
+//           </form>
+//       </div>
+      
+//     </>
+//   );
+// }
